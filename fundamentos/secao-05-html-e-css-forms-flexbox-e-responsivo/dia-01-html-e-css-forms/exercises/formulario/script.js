62f4453b5cecf854f2867c6737c47e28fdf324ae
Josue Azevedo <josuerodriguers03@gmail.com>
2023-05-19 20:28:27 -0300

    formulário simples exercicio

---
+++ fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercises/formulario/script.js
@@ -0,0 +1,13 @@
+// SELETORES
+const inputText = document.querySelector("#input-text");
+const inputCheckbox = document.querySelector("#input-checkbox");
+const hrefLink = document.querySelector("#href");
+
+
+    hrefLink.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+
+    inputCheckbox.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
